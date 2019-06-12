7d8d3ce2fe20ad6d7964f859659c6b6f83b1c909
Vishwa Shankar <vishwas1020@gmail.com>
2019-06-12 22:51:55 +0530

    palindrome number python solution

---
+++ Leetcode/Easy/palindrome_num.py
@@ -0,0 +1,8 @@
+def isPalindrome(x: int) -> bool:
+        if x<0 or (x%10==0 and x!=0):
+            return False
+        reverse_num = 0
+        while(x>reverse_num):
+            reverse_num= reverse_num*10 + (x%10)
+            x//=10
+        return x==reverse_num or x==reverse_num//10
